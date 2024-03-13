5617e442536f9f957d482041582090837f711016
OMAR ABARRA <abarraomar@gmail.com>
2024-03-13 10:04:47 +0000

    Votre message de commit ici

---
+++ zip.py
@@ -0,0 +1,10 @@
+usernames = ["omar","anas","mohamed","yassin"]
+
+passwords ={"12345","kdkd444","#$%Fff","kkdk444!@#"}
+
+users = zip(usernames,passwords)
+
+print(type(users))
+
+for i in users:
+    print(i)
